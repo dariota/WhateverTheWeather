04bf32aed79cdfed81e1fc76c578b6349d80ae4a
Dário Tavares Antunes <dario@ntun.es>
2019-04-23 01:02:19 +0100

    Add colours and tidy up types

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,12 @@
 module Main where
 
 import DarkSky
-import Data.Maybe (fromJust, isJust, fromMaybe, maybe)
-import qualified Data.Text as T
+import Console
+import Data.Maybe (fromJust, isJust, fromMaybe)
+import qualified Data.List as L
+import qualified Data.Text as T (Text, pack, unpack, concat)
+import qualified System.Console.ANSI as C
+import qualified System.Console.ANSI.Types as CT
 import qualified Data.ByteString.Lazy as B
 import Data.Aeson
 
@@ -12,49 +16,68 @@ main :: IO ()
 main = do
   json <- B.readFile "test/data/now.json"
   let response         = fromJust $ decode json
-      tempDescription  = maybe "" tempString $ currently response
-      warnDescription  = maybe "" alertString $ alerts response
-      rainDescription  = maybe "" precipString $ currently response
-      cloudDescription = maybe "" cloudString $ currently response
-      fullString       = T.intercalate ", " $ filter (/= "") [tempDescription, cloudDescription, rainDescription, warnDescription]
-  writeFile "output.txt" $ T.unpack $ T.concat [fullString, "\n"]
+      tempDescription  = tempString $ currently response
+      warnDescription  = alertString $ alerts response
+      rainDescription  = precipString $ currently response
+      cloudDescription = cloudString $ currently response
+      fullString       = concat $ (L.intersperse [commaOut] $ filter (not . null) [tempDescription, cloudDescription, rainDescription, warnDescription]) ++ [[Output "\n" BrightWhite]]
+  mapM_ evaluateOutput fullString
 
-cloudString :: Point -> T.Text
-cloudString point =
+evaluateOutput :: Output -> IO ()
+evaluateOutput output = do
+  let (content, colour, intensity) = unpackOutput output
+  C.setSGR [CT.SetColor CT.Foreground intensity colour]
+  putStr $ T.unpack content
+  C.setSGR [CT.Reset]
+
+cloudString :: Maybe Point -> [Output]
+cloudString Nothing = []
+cloudString (Just point) =
   case cloudCover point of
-    Just cover -> coverString cover
-    Nothing    -> ""
+    Just cover -> [coverString cover]
+    Nothing    -> []
 
-coverString :: Float -> T.Text
+coverString :: Float -> Output
 coverString cover
-  | cover > 0.75 = "overcast"
-  | cover > 0.50 = "cloudy"
-  | otherwise    = "clear"
+  | cover > 0.75 = Output "overcast" BrightBlack
+  | cover > 0.50 = Output "cloudy" White
+  | otherwise    = Output "clear" BrightBlue
 
-precipString :: Point -> T.Text
-precipString point =
+precipString :: Maybe Point -> [Output]
+precipString Nothing = []
+precipString (Just point) =
     if intensity < 0.1 then
-      ""
+      []
     else
       case kind of
-        Just precip -> T.concat [precip, " (", T.pack $ show intensity, " mm)"]
-        Nothing     -> ""
+        Just precip -> [precipDescription precip intensity]
+        Nothing     -> []
   where
     intensity = fromMaybe 0 $ precipIntensity point
     kind = precipType point
 
-tempString :: Point -> T.Text
-tempString point = T.intercalate " " $ map fromJust $
+precipDescription :: T.Text -> Float -> Output
+precipDescription kind intensity = Output (T.concat [kind, " (", T.pack $ show intensity, " mm)"]) colour
+  where
+    colour =
+      case kind of
+        "rain"  -> Blue
+        "sleet" -> White
+        "snow"  -> BrightWhite
+
+tempString :: Maybe Point -> [Output]
+tempString Nothing = []
+tempString (Just point) = map fromJust $
   if includeBoth actual apparent then
-      [actualString, apparentString]
+      [actualString, Just $ Output " " BrightWhite, apparentString]
     else
       take 1 $ filter isJust [actualString, apparentString, fallback]
   where
     actual = temperature point
-    actualString = fmap (tempCelsius False) actual
+    actualString = fmap (\x -> Output (tempCelsius False x) BrightWhite) actual
     apparent = apparentTemperature point
-    apparentString = fmap (tempCelsius True) apparent
-    fallback = Just $ tempCelsius False "?"
+    apparentString = fmap (\x -> Output (tempCelsius True x) White) apparent
+    fallback = Just $ Output (tempCelsius False "?") BrightRed
 
 includeBoth :: Maybe Float -> Maybe Float -> Bool
 includeBoth (Just a) (Just b) = (abs $ a - b) >= 1
@@ -69,18 +92,22 @@ tempCelsius wrap temp = T.concat $
   where
     middle = T.pack . show $ temp
 
-alertString :: [Alert] -> T.Text
-alertString []     = ""
-alertString alerts = T.concat ["[", T.intercalate ", " $ severityTexts, "]"]
+alertString :: Maybe [Alert] -> [Output]
+alertString Nothing       = []
+alertString (Just [])     = []
+alertString (Just alerts) = (Output "[" BrightWhite) : ((L.intersperse commaOut severityTexts) ++ [Output "]" BrightWhite])
   where
-    alertTypes = ["advisory", "watch", "warning"]
-    severityTexts = filter (/= "") $ map (severityString alerts) alertTypes
+    alertTypes = [("advisory", BrightYellow), ("watch", Yellow), ("warning", Red)]
+    severityTexts = concat $ map (severityString alerts) alertTypes
 
-severityString :: [Alert] -> T.Text -> T.Text
-severityString alerts kind =
+severityString :: [Alert] -> (T.Text, Colour) -> [Output]
+severityString alerts (kind, colour) =
   if length severities == 0 then
-      ""
+      []
     else
-      T.concat [T.pack . show $ length severities, " ", kind]
+      [Output (T.concat [T.pack . show $ length severities, " ", kind]) colour]
   where
     severities = filter ((== kind) . severity) alerts
+
+commaOut :: Output
+commaOut = Output ", " BrightWhite
--- src/Console.hs
+++ src/Console.hs
@@ -1,6 +1,8 @@
 module Console
   (
-    Output
+    Output(..),
+    Colour(..),
+    unpackOutput
   ) where
 
 import qualified Data.Text as T
@@ -9,5 +11,38 @@ import qualified System.Console.ANSI.Types as A
 data Output =
   Output {
     content :: T.Text,
-    fgColour :: A.Color
+    fgColour :: Colour
   } deriving (Show)
+
+data Colour =
+  Black | BrightBlack |
+    Red | BrightRed |
+    Green | BrightGreen |
+    Yellow | BrightYellow |
+    Blue | BrightBlue |
+    Magenta | BrightMagenta |
+    Cyan | BrightCyan |
+    White | BrightWhite
+  deriving (Show)
+
+unpackOutput :: Output -> (T.Text, A.Color, A.ColorIntensity)
+unpackOutput output = (content output, colour, intensity)
+  where
+    (colour, intensity) =
+      case fgColour output of
+        Black   -> (A.Black, A.Dull)
+        Red     -> (A.Red, A.Dull)
+        Green   -> (A.Green, A.Dull)
+        Yellow  -> (A.Yellow, A.Dull)
+        Blue    -> (A.Blue, A.Dull)
+        Magenta -> (A.Magenta, A.Dull)
+        Cyan    -> (A.Cyan, A.Dull)
+        White   -> (A.White, A.Dull)
+        BrightBlack   -> (A.Black, A.Vivid)
+        BrightRed     -> (A.Red, A.Vivid)
+        BrightGreen   -> (A.Green, A.Vivid)
+        BrightYellow  -> (A.Yellow, A.Vivid)
+        BrightBlue    -> (A.Blue, A.Vivid)
+        BrightMagenta -> (A.Magenta, A.Vivid)
+        BrightCyan    -> (A.Cyan, A.Vivid)
+        BrightWhite   -> (A.White, A.Vivid)
